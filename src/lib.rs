4ad0476568fa6693d77f1be2c723399ff23b3fc8
BIGNARF <86953676+BIGNARF@users.noreply.github.com>
2023-10-11 23:47:24 -0300

    test

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use std::vec::Vec;
+
+    pub struct OPController {
+        processing: Vec<String>,
+        scheduled: Vec<String>,
+    }
+    impl OPController {
+        pub fn new(size: u32) -> OPController {
+            let mut random_texts: Vec<String> = Vec::new();
+            for _ in 0..size {
+                let random_string = String::from("TEXT"); // Adjust the range as needed
+                random_texts.push(random_string);
+            }
+            OPController {
+                processing: random_texts,
+                scheduled: Vec::new(),
+            }
+        }
+
+        pub fn display(&self){
+            for text in &self.processing{
+                println!("{}",text);
+            }
+        }
+    }
+
+
+    pub trait Testing {
+        fn Printer(){
+            println!("test");
+        }
+    }
