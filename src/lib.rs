2d463f66226e617e9d0098ffd695599200ab7adf
BIGNARF <86953676+BIGNARF@users.noreply.github.com>
2023-10-27 22:30:01 -0300

    trait updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,23 @@
-use std::vec::Vec;
-
-    pub struct OPController {
-        processing: Vec<String>,
-        scheduled: Vec<String>,
-    }
-    impl OPController {
-        pub fn new(size: u32) -> OPController {
-            let mut random_texts: Vec<String> = Vec::new();
-            for _ in 0..size {
-                let random_string = String::from("TEXT"); // Adjust the range as needed
-                random_texts.push(random_string);
-            }
-            OPController {
-                processing: random_texts,
-                scheduled: Vec::new(),
-            }
-        }
-
-        pub fn display(&self){
-            for text in &self.processing{
-                println!("{}",text);
-            }
-        }
-    }
 
 
-    pub trait msg_bulletin{
+pub struct Message<T> {
+    receiver: String,
+    content: T,
+    //callback :fn()
+}
 
-    }
+pub trait Module {
+    //fn is_exclusive()->bool;
 
-    pub trait Module {
-        //fn is_exclusive()->bool;
+    //fn get_messages()->Box<dyn msg_bulletin>;
 
-        //fn get_messages()->Box<dyn msg_bulletin>;
+    fn start();
+  
+    //fn before_frame();
 
-        fn start();
+    fn process_frame();
 
-        //fn before_frame();
+    //fn after_frame();
 
-        //fn after_frame();
-
-        fn finish();
-    }
+    fn finish();
+}
